f5a082cbef8ab7aef1403003f82a8e8801af4fba
Daniela Engert <dani@ngrt.de>
2015-03-15 09:41:07 +0100

    Fix compiler warnings about unused formal parameters.
    Signed-off-by: Daniela Engert <dani@ngrt.de>

--- test/rolling_moment.cpp
+++ test/rolling_moment.cpp
@@ -74,7 +74,7 @@ void test_rolling_fifth_moment()
 ///////////////////////////////////////////////////////////////////////////////
 // init_unit_test_suite
 //
-test_suite* init_unit_test_suite( int argc, char* argv[] )
+test_suite* init_unit_test_suite( int, char*[] )
 {
     test_suite *test = BOOST_TEST_SUITE("rolling moment test");
 
--- test/weighted_sum_kahan.cpp
+++ test/weighted_sum_kahan.cpp
@@ -53,7 +53,7 @@ void test_weighted_sum_of_variates_kahan()
 ///////////////////////////////////////////////////////////////////////////////
 // init_unit_test_suite
 //
-test_suite* init_unit_test_suite( int argc, char* argv[] )
+test_suite* init_unit_test_suite( int, char*[] )
 {
     test_suite *test = BOOST_TEST_SUITE("weighted sum kahan tests");
 
